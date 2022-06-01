831342c49d83a86ad3ac8ba683766d89727f899e
emanliis47 <98544759+emanliis47@users.noreply.github.com>
2022-06-02 00:22:38 +0530

    So Finally I DONE IT MYSELF

---
+++ src/logger/logger.js
@@ -0,0 +1,7 @@
+
+const welcome = function() {
+    console.log('Welcome to my application. I am SameerSweety and a part of FunctionUp Thorium cohort')
+}
+
+
+module.exports.welcome = welcome
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,30 +1,25 @@
 const express = require('express');
-const externalModule = require('./logger')
+const externalModule = require('../logger/logger')
+const module2=require('../util/helper')
+const module3=require('../validator/formatter.js')
 
 const router = express.Router();
 
 router.get('/test-me', function (req, res) {
-    console.log('The constant in logger route has a value '+externalModule.endpoint)
-    console.log('The current batch is '+externalModule.batch)
-    externalModule.log()
-    res.send('My first ever api!')
+    externalModule.welcome()
+    module2.PrintDate()
+    module2.PrintMonth()
+    module2.getBatchInfo()
+    module3.spaceTrim()
+    module3.toLowerCh()
+    module3.toUpperCh()
+    res.send('Welcome to my application. I am SameerSweety and a part of FunctionUp Thorium cohort')
 });
 
-router.get('/test-me1', function (req, res) {
-    res.send('My second ever api!')
-});
 
-router.get('/test-me2', function (req, res) {
-    res.send('My third api!')
-});
 
-router.get('/test-me3', function (req, res) {
-    res.send('My 4th api!')
-});
 
-router.get('/test-me4', function (req, res) {
-    res.send('My last api!')
-});
+
 
 module.exports = router;
 // adding this comment for no reason
