78ad29046df258173067024973599d988119595a
emanliis47 <98544759+emanliis47@users.noreply.github.com>
2022-06-02 20:58:50 +0530

    Find Missing No

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,9 +1,7 @@
 const express = require('express');
 const myHelper = require('../util/helper')
 const underscore = require('underscore')
-
 const router = express.Router();
-
 router.get('/test-me', function (req, res) {
     myHelper.printDate()
     myHelper.getCurrentMonth()
@@ -16,6 +14,31 @@ router.get('/test-me', function (req, res) {
 router.get('/hello', function (req, res) {
    
     res.send('Hello there!')
+});
+router.get("/sol2", function (req, res) {
+    let arr= [33, 34, 35, 37, 38]
+    function find_Missing1(arr){
+        let sum1 = 0
+        for(let y in arr ){
+          sum1 += arr[y]
+        }
+        let z = arr.length + 1;
+        Actual_Sum1 = Math.floor((z*(z+1))/2);
+        return console.log(Actual_Sum1 - sum1)}
+    res.send("Hi I Am Confused");
+});
+router.get("/sol1", function (req, res) {
+    let a = [1, 2, 3, 5, 6, 7]
+    function find_Missing(a){
+  let sum = 0
+  for(let x in a ){
+    sum += a[x]
+  }
+  let y = a.length + 1;
+  Actual_Sum = Math.floor((y*(y+1))/2);
+  return console.log(Actual_Sum - sum)
+}
+    res.send("Thanks");
 });
 
 router.get('/candidates', function(req, res){
