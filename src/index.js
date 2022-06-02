08b4cd7564362cef24b5e2387fa7763b40e1a49e
emanliis47 <98544759+emanliis47@users.noreply.github.com>
2022-06-02 15:17:32 +0530

    YesterDay Updation Copy Of Assignment

--- src/index.js
+++ src/index.js
@@ -8,9 +8,9 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use('/', route);
+app.use('/', route);/* '/'- It's Define The Processing After This - 270.0.0.1||Localhost:PORT||3000 && route- Define The Path Where it Prosseces */
 
 app.listen(process.env.PORT || 3000, function() {
     console.log('Express app running on port ' + (process.env.PORT || 3000))
 });
-
+// PORT|| 3000 - It's Define The Port No 
--- src/routes/route.js
+++ src/routes/route.js
@@ -2,7 +2,8 @@ const express = require('express');
 const externalModule = require('../logger/logger')
 const module2=require('../util/helper')
 const module3=require('../validator/formatter.js')
-
+const underscore =require('underscore')
+const lodash = require('lodash')
 const router = express.Router();
 
 router.get('/test-me', function (req, res) {
@@ -13,13 +14,20 @@ router.get('/test-me', function (req, res) {
     module3.spaceTrim()
     module3.toLowerCh()
     module3.toUpperCh()
+    const namelist = underscore.first(["SameerSweety", "Swain", "KareenaKanha", "Swain"])
+    console.log(namelist)
     res.send('Welcome to my application. I am SameerSweety and a part of FunctionUp Radon cohort')
 });
-
-
-
-
-
-
+router.get('/hello', function(req, res){
+const arrayChunk = lodash.chunk(["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],1)
+console.log(arrayChunk)
+const arrayTail = lodash.tail([1, 3, 5, 7, 9, 11, 13, 15, 17, 19])
+console.log(arrayTail)
+const arrayUnion = lodash.union([1, 2], [1, 2, 3], [1,, 2, 3, 4], [1, 2, 3, 4, 5], [1, 2, 3, 4, 5, 6])
+console.log(arrayUnion)
+const ArrayPair = lodash.fromPairs([["Horror", "The Shining"], ["drama","Titanic"], ["thriller","Shutter Island"], ["fantasy","Pans Labyrinth"]])
+console.log(ArrayPair)
+res.send('Welcome To My Application.')
+})
 module.exports = router;
 // adding this comment for no reason
