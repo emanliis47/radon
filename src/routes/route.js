de15b8437c8874d3f5212476a2ce3dfac615cf25
emanliis47 <98544759+emanliis47@users.noreply.github.com>
2022-06-03 13:25:05 +0530

    OneDay I Will

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,64 +1,33 @@
 const express = require('express');
-const myHelper = require('../util/helper')
-const underscore = require('underscore')
 const router = express.Router();
-router.get('/test-me', function (req, res) {
-    myHelper.printDate()
-    myHelper.getCurrentMonth()
-    myHelper.getCohortData()
-    let firstElement = underscore.first(['Sabiha','Akash','Pritesh'])
-    console.log('The first element received from underscope function is '+firstElement)
-    res.send('My first ever api!')
+
+/*Create an API for GET /movies that returns a list of movies. Define an array of movies in your code and return the value in response.*/
+
+router.get('/movies', function(req, res){
+    const movieList = ["A Perfect Pairing", "Senior Year", "Jackass 4.5", "Dangerous", "	Sonic the Hedgehog", "	Top Gun", "Cleveland Abduction", "Turbo"]
+    res.send(movieList)
 });
 
-router.get('/hello', function (req, res) {
-   
-    res.send('Hello there!')
-});
-router.get("/sol2", function (req, res) {
-    let arr= [33, 34, 35, 37, 38]
-    function find_Missing1(arr){
-        let sum1 = 0
-        for(let y in arr ){
-          sum1 += arr[y]
-        }
-        let z = arr.length + 1;
-        Actual_Sum1 = Math.floor((z*(z+1))/2);
-        return console.log(Actual_Sum1 - sum1)}
-    res.send("Hi I Am Confused");
-});
-router.get("/sol1", function (req, res) {
-    let a = [1, 2, 3, 5, 6, 7]
-    function find_Missing(a){
-  let sum = 0
-  for(let x in a ){
-    sum += a[x]
-  }
-  let y = a.length + 1;
-  Actual_Sum = Math.floor((y*(y+1))/2);
-  return console.log(Actual_Sum - sum)
-}
-    res.send("Thanks");
-});
+/*Create an API GET /movies/:indexNumber (For example GET /movies/1 is a valid request and it should return the movie in your array at index 1). You can define an array of movies again in your api */
 
-router.get('/candidates', function(req, res){
-    console.log('Query paramters for this request are '+JSON.stringify(req.query))
-    let gender = req.query.gender
-    let state = req.query.state
-    let district = req.query.district
-    console.log('State is '+state)
-    console.log('Gender is '+gender)
-    console.log('District is '+district)
-    let candidates = ['Akash','Suman']
-    res.send(candidates)
-})
+router.get('/movies/:indexNumber', function(req, res){
+    const movieOfList = ["A Perfect Pairing", "Senior Year", "Jackass 4.5", "Dangerous", "	Sonic the Hedgehog", "	Top Gun", "Cleveland Abduction", "Turbo"]
+    var i = req.params.indexNumber
+    var j = i - 1
+    if(i <= movieOfList.length){
+        res.send(movieOfList[j])
+        
+            } else{
+                res.send("Invalid List")
+            }
+}); 
 
-router.get('/candidates/:canidatesName', function(req, res){
-    console.log('The request objects is '+ JSON.stringify(req.params))
-    console.log('Candidates name is '+req.params.canidatesName)
-    res.send('Done')
-})
+router.get('/films', function(req, res){
+    const movieComp = [{Name:"A Perfect Pairing", id: 1}, {Name:"Senior Year", id: 2}, {Name:"Jackass 4.5", id: 3}, {Name:"Dangerous", id: 4}, {Name:"Sonic the Hedgehog", id: 5}];
+    res.send(movieComp)
+}); 
 
+       
 
 module.exports = router;
 // adding this comment for no reason
