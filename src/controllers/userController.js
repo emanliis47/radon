bc9d714cc56b6aefe9053ba4c983587f45f4074b
emanliis47 <98544759+emanliis47@users.noreply.github.com>
2022-06-07 04:45:15 +0530

    BookAPI

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,27 +1,19 @@
 const UserModel= require("../models/userModel")
 
-const createUser= async function (req, res) {
-    let data= req.body
-    let savedData= await UserModel.create(data)
-    res.send({msg: savedData})
-}
-
-const getUsersData= async function (req, res) {
-    let allUsers= await UserModel.find()
-    res.send({msg: allUsers})
-}
+let a = 12
+let b = a+ 10
+console.log(b) 
 const createBookName = async function (req, res){
     let bookData = req.body
     let saveBookData = await UserModel.create(bookData)
-    res.send({msg: saveBookData})
+    res.send({msg2: saveBookData})
 }
 
 const getBookData= async function (req, res) {
     let allBook= await UserModel.find()
-    res.send({msg: allBook})
+    res.send({msg1: allBook})
 }
 
 module.exports.createBookName = createBookName
-module.exports.createUser= createUser
-module.exports.getUsersData= getUsersData
+
 module.exports.getBookData= getBookData
