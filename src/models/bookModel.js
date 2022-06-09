8a254c75a17ceede5fb61b608ab22ddb5bb073d2
emanliis47 <98544759+emanliis47@users.noreply.github.com>
2022-06-09 07:33:00 +0530

    Updated MongoDB @SameerSweety At 7:30 AM

--- src/models/bookModel.js
+++ src/models/bookModel.js
@@ -1,31 +1,10 @@
 const mongoose = require('mongoose');
+const bookSchema = new mongoose.Schema({
+    name: String,
+    author_id:Number,
+    price:Number,
+    ratings: String,
+},
 
-const bookSchema = new mongoose.Schema( {
-    bookName: String, 
-    authorName: String, 
-    tags: [String],
-    
-    isPublished: Boolean,
-    prices: {
-        indianPrice: String,
-        europePrice: String,
-    },
-    sales: {type: Number, default: 10}
-}, { timestamps: true });
-
-
-module.exports = mongoose.model('Book', bookSchema) //users
-
-//Validation:
-//require:true
-//unique
-// default
-
-//String
-//Number
-//Date
-//Boolean
-// Arrays
-// Object
-// ObjectId
-// Buffer - not cover
+{timestamps: true })
+module.exports = mongoose.model('Books', bookSchema)
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,19 +1,12 @@
 const express = require('express');
 const router = express.Router();
-// const UserModel= require("../models/userModel.js")
-const UserController= require("../controllers/userController")
-const BookController= require("../controllers/bookController")
+const BookController= require("../controllers/bookController");
 
-router.get("/test-me", function (req, res) {
-    res.send("My first ever api!")
-})
-
-router.post("/createUser", UserController.createUser  )
-
-router.get("/getUsersData", UserController.getUsersData)
-
-router.post("/createBook", BookController.createBook  )
-
-router.get("/getBooksData", BookController.getBooksData)
-
+router.get("/findBook/:Author_Ids", BookController.findBook)
+router.post("/createBook", BookController.createBook)
+router.post("/createAuthor", BookController.createAuthor)
+router.get("/chetanBook", BookController.getChetanBhagat)
+router.get("/getUpadate", BookController.authorBooks)
+router.get("/bookPrice", BookController.bookPrice)
+router.get("/FindAge", BookController.FindAge)
 module.exports = router;
